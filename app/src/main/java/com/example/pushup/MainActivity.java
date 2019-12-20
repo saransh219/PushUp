fd11150ee04785b00c3b685e81cb8f3e865425b2
Saransh Jain <35698657+saransh219@users.noreply.github.com>
2019-12-21 01:40:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pushup/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.pushup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    private void testUpload()
+    {
+        int i =0;
+        i= i+1;
+    }
+
+}
